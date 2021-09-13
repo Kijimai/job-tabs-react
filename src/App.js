5bbd1967751721d5bddbc65df82608746186d31a
Jibbyworks <JDBucog@yahoo.com>
2021-09-13 14:36:14 -0700

    fetch api using axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { FaAngleDoubleRight } from "react-icons/fa"
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project"
 function App() {
-  return <h2>tabs project setup</h2>
+  const [isLoading, setIsLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setJobs(res.data)
+        setIsLoading(false)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  if (isLoading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    )
+  } else {
+    return (
+      <main>
+        <section className="section">
+          <div className="title">
+            <h2>JOBS</h2>
+            <div className="underline"></div>
+          </div>
+        </section>
+        <div className="jobs-center">
+          {jobs.map(job => {
+            return <h3>{job.company}</h3>
+          })}
+        </div>
+      </main>
+    )
+  }
 }
 
 export default App
