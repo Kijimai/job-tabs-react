ef98226fe97af01ae56d4fca351f4f24a79387b3
Jibbyworks <JDBucog@yahoo.com>
2021-09-13 23:23:35 -0700

    finish project

--- src/App.js
+++ src/App.js
@@ -25,23 +25,48 @@ function App() {
         <h1>Loading...</h1>
       </section>
     )
-  } else {
-    return (
-      <main>
-        <section className="section">
-          <div className="title">
-            <h2>JOBS</h2>
-            <div className="underline"></div>
-          </div>
-        </section>
-        <div className="jobs-center">
-          {jobs.map(job => {
-            return <h3>{job.company}</h3>
+  }
+
+  //This destructure must occur after the loading conditional to avoid errors
+  const { company, dates, duties, title } = jobs[value]
+  return (
+    <main>
+      <section className="section">
+        <div className="title">
+          <h2>My Experience</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button
+                key={job.id}
+                onClick={() => setValue(index)}
+                class={`job-btn ${index === value && 'active-btn'} `}
+              >
+                {job.company}
+              </button>
+            )
           })}
         </div>
-      </main>
-    )
-  }
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </main>
+  )
 }
 
 export default App
